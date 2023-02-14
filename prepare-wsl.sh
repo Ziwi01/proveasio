09a9cf403d196cb51a4330b6993684616da41b57
Eryk Kozakiewicz <Eryk.Kozakiewicz@igt.com>
2023-02-14 18:22:39 +0100

    fix: use apt-get instead of apt

--- prepare-wsl.sh
+++ prepare-wsl.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "Updating apt..."
-apt update
+apt-get update
 echo "Upgrading system..."
-apt upgrade -y
+apt-get upgrade -y
 echo "Installing ansible, python3 and required PIP modules..."
-apt install -y libkrb5-dev
-apt install -y ansible aptitude python3-dev python3-pip python3-setuptools python3-venv
+apt-get install -y libkrb5-dev
+apt-get install -y ansible aptitude python3-dev python3-pip python3-setuptools python3-venv
 python -m pip install --user "pywinrm>=0.3.0"
 python -m pip install --user "pywinrm[credssp]"
 python -m pip install --user "pywinrm[kerberos]"
