89197cde55dfb79015faddc93a01a1a70cff30ae
Proveasio <Proveasio@hell.no>
2024-04-23 02:14:27 +0200

    fix(lunarvim): Prevent double dash on enter in lists for Markdown files

--- ansible/roles/config/files/lvim/lua/user/general.lua
+++ ansible/roles/config/files/lvim/lua/user/general.lua
@@ -27,6 +27,13 @@ vim.g.vim_markdown_folding_disabled = 1
 -- `ge` to follow anchor links
 vim.g.vim_markdown_follow_anchor = 1
 
+-- Prevent double list markers (-) on enter
+vim.g.vim_markdown_auto_insert_bullets = 0
+vim.g.vim_markdown_new_list_item_indent = 0
+
+-- Let MKDX plugin handle indentation
+vim.g['mkdx#settings.insert_indent_mappings'] = 1
+
 -- Enable transparency
 -- lvim.transparent_window = true
 
